0f233ad720aa51d8e080bc1e278efe24e7a46c7f
chepaksov <chepaksov@mail.ru>
2023-02-16 22:42:49 +0300

    5.1.5

--- src/main/java/com/bank/example/model/Tariff.java
+++ src/main/java/com/bank/example/model/Tariff.java
@@ -4,12 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -21,7 +16,8 @@ import java.util.Objects;
 public class Tariff {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tariff_sequence")
+    @SequenceGenerator(sequenceName = "tariff_sequence", name = "tariff_generator", allocationSize = 999)
     private Long id;
 
     private String name;
